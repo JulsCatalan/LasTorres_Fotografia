84e440d25da5c7ad905390eada733115ce5c00b8
JulsCatalan <julscatalan@gmail.com>
2023-10-26 00:54:38 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.querySelector('.container').style.right = '0%';
+    document.querySelector('.menu').style.opacity = '1';
+}
+
+function closeMenu() {
+    document.querySelector('.container').style.right = '-100%';
+    document.querySelector('.menu').style.opacity = '0';
+}
